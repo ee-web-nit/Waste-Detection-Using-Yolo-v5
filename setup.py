75102b4eb7f1676a73c0c19e9f48e84ae517785d
Mohammed Bilal <mohammed_b230057ee@nitc.ac.in>
2026-06-07 17:41:01 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='WasteDetection',
+    version='0.0.0',
+    author='Bilal',
+    author_email='<mohammed_b230057ee@nitc.ac.in>',
+    packages=find_packages(),
+    install_requires=[]
+)
